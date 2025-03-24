78a27133cdee3f77b80beb98447b5e915d49004d
Felix Gnass <fgnass@gmail.com>
2025-03-24 09:55:23 +0100

    fix: add subfolders property

--- packages/decap-cms-core/src/types/redux.ts
+++ packages/decap-cms-core/src/types/redux.ts
@@ -371,6 +371,7 @@ export type CmsCollection<IsInternal extends boolean = true> = {
   publish?: boolean;
   nested?: {
     depth: number;
+    subfolders?: boolean;
   };
   meta?: { path?: CmsFieldMeta<IsInternal> };
 
